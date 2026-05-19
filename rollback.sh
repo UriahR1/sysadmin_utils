b7dd2804dc2cf53a101f1c74b29ff60f7c7cac75
UriahR1 <uriahredelinghuys@gmail.com>
2026-05-19 09:44:52 +0200

    Updated rollback.sh script

--- rollback.sh
+++ rollback.sh
@@ -1,8 +1,49 @@
 #!/usr/bin/env bash
 
-# Sourcing config.envrelative to file path
-source "$(dirname "$0"/config.env)"
 
-find "$LOG_DIR" -name ""
+set -euo pipefail
 
+# Sourcing config.env relative to file path
 
+source "$(dirname "$0")/config.env"
+
+# Storing last 5 files found in backup directory to rollback to be used as an array
+
+mapfile -t rollback < <(find "$BACKUP_DIR" -name "*.tar.gz" | xargs ls -t | head -n 5)
+
+# Testing for an empty backups diretory 
+
+if [ "${#rollback[@]}" -lt 1 ]; then
+	
+	echo "Error, no backup found"
+
+elif [ "${#rollback[@]}" -gt 0 ]; then
+	
+	while true; do
+	
+	read -p "Enter the file that you would like to roll back to (numerical input 1-5) : " num
+	
+	if [[ ! "$num" =~ ^[1-5]$ ]]; then
+	
+		echo "Error, invalid input"
+	
+	fi
+	
+	break
+	
+elif [[ "$num" =~ ^[1-5]$ ]]; then
+	    
+		echo "File number $num will be restored"
+      	
+		while true; do
+
+	read -p "Enter the full path where you would like to restore :" restorepoint
+	
+	if [ ! -d "$restorepoint" ]; then
+		
+		echo "Error : could not find the path : $restorepoint
+
+	elif [ -d "$restorepoint" ]; then
+
+	for i in "${!rollback[@]}"; do
+		echo "$((i+1)): $(basename "${rollback[$i]}")"
--- rotate.sh
+++ rotate.sh
@@ -9,10 +9,11 @@ source "$(dirname "$0")/config.env"
 filecount=$(find "$BACKUP_DIR" -name "*.tar.gz" -type f | wc -l)
 
 if [[ "$filecount" -gt "$MAX_BACKUPS" ]]; then
+	
 	oldest=$(ls -t "$BACKUP_DIR"/*.tar.gz | tail -n 1)
 	oldest_timestamp=$(basename -s ".tar.gz" "$oldest")
-	
 	oldlog=$(find "$LOG_DIR" -name "*_$oldest_timestamp.log")         
+	
 	while true; do
 
 	read -p "File $oldest and corresponding logfile to be removed, are you sure [Y/n] :" answer
@@ -20,12 +21,18 @@ if [[ "$filecount" -gt "$MAX_BACKUPS" ]]; then
 	if [[ "$answer" =~ ^[Yy]$ ]]; then
 
 	rm "$oldest"
-	[[ -f "$oldlog" ]] && rm "$oldlog"
-	echo "Operation complete : $oldest and $oldlog removed"
+	
+	[[ -f "$oldlog" ]] && rm "$oldlog" 
+	
+	echo "Operation complete : File : $oldest and File : $oldlog removed"
+	
 	break
 else
+	
 	echo "Cancelled..."
+	
 	break
+	
 	fi
 done
 	
